97d4cd046f8a28037cdf38e9293f4933d8ad3416
rodi10 <38446195+rodi10@users.noreply.github.com>
2019-11-29 20:43:10 +0700

    histori udah ya tinggal di hubungin ke datanya, sm detail udah aku buat cuma belum aku inten ke historinya. makasih

--- app/src/main/java/com/example/tubes_cekpotensirisikojantung/MainActivity.java
+++ app/src/main/java/com/example/tubes_cekpotensirisikojantung/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.tubes_cekpotensirisikojantung;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.tubes_cekpotensirisikojantung.ui.history.histori;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.example_item);
+        Intent i = new Intent(MainActivity.this , histori.class);
+        startActivity(i);
     }
+
 }
---
+++ app/src/main/java/com/example/tubes_cekpotensirisikojantung/ui/history/detail.java
@@ -0,0 +1,16 @@
+package com.example.tubes_cekpotensirisikojantung.ui.history;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tubes_cekpotensirisikojantung.R;
+
+public class detail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
